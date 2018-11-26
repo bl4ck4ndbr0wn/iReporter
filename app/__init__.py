b2940bf4cda3497df6db675da090adc901773d8e
bl4ck4ndbr0wn <alphandirangu@gmail.com>
2018-11-26 23:20:37 +0300

    Project setup file structure

---
+++ app/__init__.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_restful import Api
+from instance.config import config
+
+from app.api.v1.views.main import AllRecords
+
+
+def create_app(config_name):
+    """
+    Create a Flask application using the app factory pattern.
+
+    :return: Flask app
+    """
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    # Add blueprints
+    from app.api import api_bp as api_blueprint
+    api = Api(api_blueprint)
+
+    # Register blueprint
+    app.register_blueprint(api_blueprint, url_prefix="/api/v1")
+
+    # Url Routes
+    routes(api)
+
+
+    return app
+
+
+def routes(api):
+    api.add_resource(AllRecords, "/")
---
+++ app/api/v1/views/main.py
@@ -0,0 +1,6 @@
+from flask_restful import Resource
+
+
+class AllRecords(Resource):
+  def get(self):
+    pass
